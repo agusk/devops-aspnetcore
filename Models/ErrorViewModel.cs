4251dcd7fae85afe52038de76bef5bc30ab9e22f
Agus Kurniawan <agusk@users.noreply.github.com>
2023-05-29 13:05:26 +0700

    updated

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace devops_aspnetcore.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
